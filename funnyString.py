c80553fef74959a1c4cb6840f477b6570cb59dcc
Hakeem Manwahab <hakeemmwh2@gmail.com>
2025-03-02 22:06:43 +0700

    first funnystring

---
+++ funnyString.py
@@ -0,0 +1,9 @@
+def funnyString(s):
+    sr = s[::-1]
+    for i in range(1,len(s)):
+        if abs(ord(s[i])-ord(s[i-1])) != abs(ord(sr[i])-ord(sr[i-1])):
+            return 'Not Funny'
+    return 'Funny'
+
+print(funnyString('acxz'))
+print(funnyString('bcxz'))  
